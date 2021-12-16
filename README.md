0f93abb1f2afe4a3ed5be87392eb5d7e10eab4e8
loris <loris.giann@live.it>
2021-12-16 17:31:57 +0100

    training directory uploaded, with related istructions in markdown file

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# face_mask_detection_proj
-This is the sistemi digitali project consisting in an android app capable of detecting the presence or absence of covid19 masks on human faces
+# Face mask detection
+## training the model
+
+In this project we took advantage of the [Object detection API](https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/index.html) from TensorFlow 2.
+To train this model follow the steps explained here.
+
+#### Download and prepare the data
+Downoad this repo:
+```sh
+git colone https://github.com/LorisGiann/face_mask_detection_proj.git
+```
+Download the Mask Dataset} from [Kaggle](https://www.kaggle.com/andrewmvd/face-mask-detection) or from (makeml site)[https://makeml.app/datasets/mask] .
+Now unpack the archive, you'll notice that for each image in _images_ there is a correspondong .xml file in _annotations_. We need to populate the _train_ and _test_ folders in _tensorflow_training/TensorFlow/workspace/training_demo/images/_, so grab a large portion of the photos with the respective xml files, and put them in the _train_ folder. Then copy the remaining files to the _test_ folder.
+
+#### Install packages
+this is a summary of the [installation page of the object detection api]([https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/install.html).
+cd to _tensorflow_training_ and follow theese steps:
+```sh
+cd tensorflow_training
+virtualenv -p python3 venv #creates a virtual environment
+source venv/bin/activate
+pip install --ignore-installed --upgrade tensorflow==2.7.0
+python -c "import tensorflow as tf;print(tf.reduce_sum(tf.random.normal([1000, 1000])))" #test tensorflow: may report warnings and info, but terminate correctly
+```
+We now need to install protobuf. To do that refer to [this paragraph](https://tensorflow-object-detection-api-tutorial.readthedocs.io/en/latest/install.html#protobuf-installation-compilation) of the installation page.
+If you are on ubuntu, you can set the invironment with the following commands (please chenge _BASE_PROTOC_DIR_ to match the location of your binary):
+```sh
+BASE_PROTOC_DIR='/home/loris/bin/google_protobuf/protoc-3.19.1-linux-x86_64'
+PROFILE_FILE='export PATH='"$BASE_PROTOC_DIR/bin"':$PATH'"\\\n"
+sudo bash -c 'echo -e '"$PROFILE_FILE"' > /etc/profile.d/google_protobuf.sh'
+```
+Now logout, login and test the setup by typing "protoc" on a terminal: a usage screen should appear. You can also run the following commands to make extra sure things are working properly with protobuf:
+```sh
+cd TensorFlow/models/research
+protoc object_detection/protos/*.proto --python_out=. #test protobuf
+cd -
+```
+Now we install pycoctools, which will be useful to benchmark the network at finding objects.
+```sh
+cd TensorFlow/models/research
+git clone https://github.com/cocodataset/cocoapi.git #already included in this repo
+cd cocoapi/PythonAPI
+make
+cp -r pycocotools ../..
+cd ../..
+python -m pip install --use-feature=2020-resolver .
+python object_detection/builders/model_builder_tf2_test.py #test the installation
+```
+#### Generating tfrecords
+Tensorflow object detection api needs image files and labels to be in a .tfrecord file format. We can use the custom modified script to generate those starting from the train and test folders we previously filled up.
+```sh
+cd TensorFlow/workspace/training_demo
+python generate_tfrecord.py -x images/train -l annotations/label_map.pbtxt -o annotations/train.record
+python generate_tfrecord.py -x images/test -l annotations/label_map.pbtxt -o annotations/test.record
+```
+NOTE: this scripts also crops and add paddigng to images to make them 230x230 (which is not done in the original script in the guide).
+You can see a few images inside those files by using the _analyze_ script (just modify the .tfrecord file and the image index at the beginning of the script).
+```sh
+#sudo apt-get install python3-tk  #you may need this
+python3 analyze.py
+```
+
+#### Training process
+It's time to train our network! Use the command:
+```sh
+#cd TensorFlow/workspace/training_demo
+python model_main_tf2.py --model_dir=models/ssd_mobilenet_v2_fpnlite_320x320_coco --pipeline_config_path=models/ssd_mobilenet_v2_fpnlite_320x320_coco/pipeline.config
+```
+You can monitor the progress of your network using pycocotools and/or Tensorboard:
+```sh
+python model_main_tf2.py --model_dir=models/ssd_mobilenet_v2_fpnlite_320x320_coco --pipeline_config_path=models/ssd_mobilenet_v2_fpnlite_320x320_coco/pipeline.config --checkpoint_dir=models/ssd_mobilenet_v2_fpnlite_320x320_coco
+tensorboard --logdir=models/ssd_mobilenet_v2_fpnlite_320x320_coco --bind #http://localhost:6006/
+```
+#### Exporting the model
+To export the model in pb format, use the command:
+```sh
+python ./exporter_main_v2.py --input_type image_tensor --pipeline_config_path ./models/ssd_mobilenet_v2_fpnlite_320x320_coco/pipeline.config --trained_checkpoint_dir ./models/ssd_mobilenet_v2_fpnlite_320x320_coco/ --output_directory ./exported-models/my_model
+```
+#### Export in tflite format
+To export the model in tflite format, you first need to use this command (it generates an intermediate SavedModel which can then be used with the TFLite Converter)
+```sh
+python export_tflite_graph_tf2.py --pipeline_config_path=./models/ssd_mobilenet_v2_fpnlite_320x320_coco/pipeline.config --trained_checkpoint_dir=./models/ssd_mobilenet_v2_fpnlite_320x320_coco/ --output_directory=./exported-models/my_model_tflite
+```
+Once this is done, we install a python package, then we can use a ptyhon3 shell to paste the blocks of code presented here:
+```sh
+pip install tflite-support
+python3
+```
+This is the python code (chenge the path constant in capital letters if you need). The script can also be adapted to use quantization, or other options, see this [google colab sketch](https://colab.research.google.com/github/tensorflow/models/blob/master/research/object_detection/colab_tutorials/convert_odt_model_to_TFLite.ipynb).
---
+++ tensorflow_training/TensorFlow/workspace/training_demo/analyze.py
@@ -0,0 +1,148 @@
+import os
+import random
+import re
+import math
+import shutil
+import pandas as pd
+from collections import namedtuple
+import tensorflow.compat.v1 as tf
+import io
+from PIL import Image
+from object_detection.utils import dataset_util, label_map_util
+import numpy as np
+
+import functools
+from object_detection import inputs
+from object_detection.core import preprocessor
+from object_detection.core import standard_fields as fields
+from object_detection.utils import config_util
+from object_detection.utils import test_case
+
+num_classes = 3
+image_num = 54
+#attenzione: se l'immagine non contine l'oggetto viene lanciata eccezione
+
+TFRecords_file = "annotations/train.record" #output file for TF record file
+
+def parse_record(record):
+    name_to_features = {
+        'image/height': tf.io.FixedLenFeature([], tf.int64),
+        'image/width': tf.io.FixedLenFeature([], tf.int64),
+        'image/filename': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'image/source_id': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'image/encoded': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'image/format': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'image/object/bbox/xmin': tf.io.VarLenFeature(dtype=tf.float32),#VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/xmax': tf.io.VarLenFeature(dtype=tf.float32),#VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/ymin': tf.io.VarLenFeature(dtype=tf.float32),#VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/ymax': tf.io.VarLenFeature(dtype=tf.float32),#VarLenFeature(dtype=tf.float32),
+        'image/object/class/text': tf.io.VarLenFeature(tf.string),
+        'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+    }
+    return tf.io.parse_single_example(record, name_to_features)
+
+def _decode_image(parsed_record):
+    """Decodes the image and set its static shape."""
+    image = tf.io.decode_image(parsed_record['image/encoded'], channels=3)
+    image.set_shape([None, None, 3])
+    return image
+
+def _decode_boxes(parsed_record):
+    """Concat box coordinates in the format of [ymin, xmin, ymax, xmax]."""
+    xmin = parsed_record['image/object/bbox/xmin'].values.numpy()
+    xmax = parsed_record['image/object/bbox/xmax'].values.numpy()
+    ymin = parsed_record['image/object/bbox/ymin'].values.numpy()
+    ymax = parsed_record['image/object/bbox/ymax'].values.numpy()
+    return zip(ymin, xmin, ymax, xmax)
+
+
+def decode_record(parsed_record):
+    image = _decode_image(parsed_record)
+    w, h = image.shape[1], image.shape[0]
+    boxes = _decode_boxes(parsed_record)
+    #fig, ax = plt.subplots()
+    #ax.imshow(image.numpy())
+    #for box in boxes:
+    #    rect = patches.Rectangle((box[1]*w, box[0]*h), (box[3]-box[1])*w, (box[2]-box[0])*h, linewidth=1, edgecolor='r', facecolor='none')
+    #    ax.add_patch(rect)
+    #plt.show()
+    tensor_dict = {
+        fields.InputDataFields.image: tf.constant(image.numpy().astype(np.float32)/255.0),
+        fields.InputDataFields.groundtruth_boxes: tf.constant(np.array(np.array(list(boxes), np.float32))),
+        fields.InputDataFields.groundtruth_classes: tf.one_hot( parsed_record['image/object/class/label'].values.numpy()-1, num_classes)
+    }
+    return tensor_dict
+
+def showImgTensor(tensor_dict):
+    image=tensor_dict[fields.InputDataFields.image]
+    boxes=tensor_dict[fields.InputDataFields.groundtruth_boxes]
+    classes=tensor_dict[fields.InputDataFields.groundtruth_classes]
+    w, h = image.shape[1], image.shape[0]
+    import matplotlib
+    matplotlib.use('TkAgg')
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    #print(tf.io.encode_png((image.numpy()*255).astype(np.uint8)).numpy()[:100])
+
+    count=0
+    for box in boxes:
+        cl=classes[count].numpy()
+        rect = patches.Rectangle((box[1]*w, box[0]*h), (box[3]-box[1])*w, (box[2]-box[0])*h, linewidth=1, edgecolor=(cl[2], cl[0], cl[1], 1), facecolor='none')
+        ax.add_patch(rect)
+        count+=1
+    plt.show()
+
+
+
+dataset = tf.data.TFRecordDataset(TFRecords_file, buffer_size=100)
+FLAGS = tf.flags.FLAGS
+#tf.disable_eager_execution()
+
+for record in dataset.take(image_num):
+    parsed_record = parse_record(record)
+
+tensor_dict=decode_record(parsed_record)
+#showImgTensor(tensor_dict)
+class DataAugmentationFnTest(test_case.TestCase):
+    def test_apply_image_and_box_augmentation(self):
+        data_augmentation_options = [
+            (preprocessor.random_horizontal_flip, { #boh sembra non funzionare
+            }),
+            #(preprocessor.resize_image, {
+            #    'new_height': 320,
+            #    'new_width': 320
+            #}),
+            (preprocessor.resize_to_range, {
+                'min_dimension': 320,
+                'max_dimension': 320,
+                'pad_to_max_dimension': False #looks like this option does not adjust the boxes positions! to add padding we use the options down here
+            }),
+            (preprocessor.random_pad_image, {
+                'min_image_size': (320,320),
+                'max_image_size': (320,320),
+                'center_pad': True
+            }),
+            #(preprocessor.random_crop_image, {
+            #    #'min_object_covered': 0.0,
+            #    'min_object_covered': 1.0,
+            #    #'aspect_ratio_range': (0.75,3.0),
+            #    #'area_range': (0.75, 1.0),
+            #    'aspect_ratio_range': (1,1),
+            #    'area_range': (1, 1),
+            #    'overlap_thresh': 0.0
+            #})
+        ]
+        data_augmentation_fn = functools.partial(inputs.augment_input_data, data_augmentation_options=data_augmentation_options)
+        augmented_tensor_dict = data_augmentation_fn(tensor_dict=tensor_dict)
+        #with self.session() as sess:
+        #    augmented_tensor_dict_out = sess.run(augmented_tensor_dict)
+        showImgTensor(augmented_tensor_dict)
+
+tf.test.main()
+
+
+
+
+
