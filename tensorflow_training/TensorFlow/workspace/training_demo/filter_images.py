e29b9567375f1583c497e212e22dc8d42b5c2510
loris <loris.giann@live.it>
2021-12-31 16:00:28 +0100

    modified filter script for new dataset

--- tensorflow_training/TensorFlow/workspace/training_demo/filter_images.py
+++ tensorflow_training/TensorFlow/workspace/training_demo/filter_images.py
@@ -11,17 +11,18 @@ import io
 from PIL import Image
 import numpy as np
 import shutil
+import gc
 
 #labels: ['balaclava_ski_mask', 'eyeglasses', 'face_no_mask', 'face_other_covering', 'face_shield', 'face_with_mask', 'face_with_mask_incorrect', 'gas_mask', 'goggles', 'hair_net', 'hat', 'helmet', 'hijab_niqab', 'hood', 'mask_colorful', 'mask_surgical', 'other', 'scarf_bandana', 'sunglasses', 'turban']
 
-VISUALIZE_IMGS = False #displays images with groundtruth boxes
-FROM = 190
-TO = 200
+VISUALIZE_IMGS = True #displays images with groundtruth boxes
+FROM = 1000
+TO = 2000
 
-COPY_IMGS_TO_DEST = True #copy the selected images to the destination path
-DEST_PATH = "/media/loris/Rock64/Toshiba500GB/tmp/face_mask_dataset_2/selectedImages"
+COPY_IMGS_TO_DEST = False #copy the selected images to the destination path
+DEST_PATH = "/home/loris/Documents/face_mask_detection_proj/tensorflow_training/TensorFlow/workspace/training_demo/tmp/selectedImages"
 
-IMG_FILE_PATH = "/media/loris/Rock64/Toshiba500GB/tmp/face_mask_dataset_2/Medical mask/Medical mask/images"
+IMG_FILE_PATH = "/media/loris/Rock64/Toshiba500GB/tmp/selectedImages"
 CSV_FILE = "/media/loris/Rock64/Toshiba500GB/tmp/face_mask_dataset_2/train.csv"
 
 imgFiles = [f for f in listdir(IMG_FILE_PATH) if isfile(join(IMG_FILE_PATH, f)) and (f.endswith('.jpg') or f.endswith('.png'))]
@@ -33,11 +34,11 @@ imageData = {filename:gb.get_group(x) for filename, x in zip(gb.groups.keys(), g
 color = {
     "face_with_mask": "green",
     "face_no_mask": "red",
-    "face_other_covering": "orange", #sometimes no covering at all! sometimes there is some covering + mask ()
+    "face_other_covering": "red", #sometimes no covering at all! sometimes there is some covering + mask ()
     "face_with_mask_incorrect": "yellow"
 }
 
-def showImgTensor(image, xminArr, yminArr, xmaxArr, ymaxArr, labelsArr):
+def showImgTensor(image, xminArr, yminArr, xmaxArr, ymaxArr, labelsArr, imageFile):
     import matplotlib
     matplotlib.use('TkAgg')
     import matplotlib.pyplot as plt
@@ -49,7 +50,12 @@ def showImgTensor(image, xminArr, yminArr, xmaxArr, ymaxArr, labelsArr):
         rect = patches.Rectangle((xminArr[i], yminArr[i]), xmaxArr[i]-xminArr[i], ymaxArr[i]-yminArr[i], linewidth=1, edgecolor=color[label], facecolor='none')
         ax.add_patch(rect)
         i+=1
-    plt.show()
+    #plt.show()
+    plt.savefig('tmp/boxes/'+imageFile, bbox_inches='tight')
+    plt.clf()
+    plt.close("all")
+    del fig
+    del ax
 
 countFile=0
 validImages=0
@@ -60,9 +66,11 @@ for imageFile in imgFiles:
     except:
         continue
     if VISUALIZE_IMGS:
-        image = np.array(Image.open(IMG_FILE_PATH+"/"+imageFile)).astype(np.float32)/255.0
-        width, height = image.shape[1], image.shape[0]
-        if countFile<FROM:
+        pilImage = Image.open(IMG_FILE_PATH+"/"+imageFile)
+        image = np.array(pilImage).astype(np.float32)/255.0
+        pilImage.close()
+        #width, height = image.shape[1], image.shape[0]
+        if countFile<=FROM:
             countFile+=1
             continue
         if countFile>TO:
@@ -89,10 +97,16 @@ for imageFile in imgFiles:
     #
     print("showing" + imageFile)
     if VISUALIZE_IMGS:
-        showImgTensor(image, xminArr, yminArr, xmaxArr, ymaxArr, labelsArr)
+        showImgTensor(image, xminArr, yminArr, xmaxArr, ymaxArr, labelsArr, imageFile)
+        del image
     if COPY_IMGS_TO_DEST:
         shutil.copyfile(IMG_FILE_PATH+"/"+imageFile , DEST_PATH+"/"+imageFile)
 
+    del xminArr
+    del yminArr
+    del xmaxArr
+    del ymaxArr
+    gc.collect()
     countFile+=1
 
 print(validImages)
